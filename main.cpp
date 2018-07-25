3fa4dba0c7b97c4fd56e72238fd045da8720b0bf
unknown <aj59bizg@gmail.com>
2018-07-25 18:51:20 -0500

    deleted comments

--- main.cpp
+++ main.cpp
@@ -5,8 +5,3 @@ int main()
 	cout << "hello world";
 	return 0;
 }
-// adding comments to test
-// this comment added in visual studio code
-// testing git through vs code
-// this should go to branch
-// should go to master
